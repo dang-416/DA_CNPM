847947a8a53bb0a3cf73cc17c335f4217c258f25
dang-416 <dangnguyen8877@gmail.com>
2023-07-13 03:08:10 +0700

    setup hosting

---
+++ src/hosting.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCcLvfkbsxFjT4qEHF-I2ct4jGj4N3gpEw",
+  authDomain: "bk-bookstore-fbd23.firebaseapp.com",
+  projectId: "bk-bookstore-fbd23",
+  storageBucket: "bk-bookstore-fbd23.appspot.com",
+  messagingSenderId: "710348586976",
+  appId: "1:710348586976:web:0401d8f9b87cca27d63585",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
